89bfcfc39e713255d686ab17be87a4e01cc2c6a8
Harmrazer <harmrazer@gmail.com>
2022-07-06 13:32:17 +0300

    task 3

--- S1/Program.cs
+++ S1/Program.cs
@@ -171,4 +171,31 @@ if (c > max) max = c;
 Console.WriteLine("max = " + max);
 */
 
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+/*
+Console.WriteLine("Task # 2 ");
+
+int a;
+
+Console.WriteLine("Input number");
+
+
+a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " is even");
+}
+
+else
+{
+    Console.WriteLine(a + " is uneven");
+}
+*/
