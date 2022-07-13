a9bb148d9a29e8eaa23a6e0fd997ea7517baa7ad
Harmrazer <108080998+Harmrazer@users.noreply.github.com>
2022-07-13 23:14:54 +0300

    Update Program.cs

--- S3/Program.cs
+++ S3/Program.cs
@@ -28,6 +28,8 @@ else
     Console.WriteLine(" Number of quart for out points is " + quart);
 
 */
+
+/*
 Console.WriteLine("Input number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
@@ -39,4 +41,26 @@ while(current <= n)
     current++;  //current = current + 1
 }
 
+*/
 
+
+//найти расстояние между двумя точками в двухмерном пространстве
+/*
+  double PantsOfPifagor(double xa, double ya, double xb, double yb)
+ {
+    double LenghtAB = Math.Sqrt((xb - xa) * (xb - xa)  +  (yb - ya) * (yb - ya));
+    return LenghtAB;
+
+ }
+Console.WriteLine("Input xa ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input xb ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input ya ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input yb ");
+int yb = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("AB is: " + PantsOfPifagor(xa, ya, xb, yb));
+
+*/
