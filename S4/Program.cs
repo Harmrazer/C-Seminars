01131763eb72624d23ca8b7650e9d697493a692c
Harmrazer <harmrazer@gmail.com>
2022-07-15 18:13:27 +0300

    s4

---
+++ S4/Program.cs
@@ -0,0 +1,88 @@
+﻿
+/*
+void FindSum()
+{
+    Console.Write("Input number > 0");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    int current = 1, sum = 0;
+    while(current <= a)
+    {
+        sum+= current;
+
+        current++;
+    }
+
+    Console.WriteLine("Sum is " + sum);
+}
+
+FindSum();
+*/
+/*
+ int FindSum(int a)
+{
+    int current = 1, sum = 0;
+    while(current <= a)
+    {
+        sum+= current;
+        current++;
+    }
+    return sum;
+}
+
+
+Console.Write("Input number > 0");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("Sum is " + FindSum(num));
+
+*/
+/*
+
+int  NumDig(int num)
+{
+    int i = 0;
+    while(num > 0)
+    {
+        
+        num = num/10;
+        i++;
+    }
+    return i;
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sum is " + NumDig(num));
+*/
+/*
+int  NumDig(int num)
+{
+    int i = 1;
+    
+    while(num > 1)
+    {
+        i = i * num;
+        num--; //num = num - 1;
+    }
+    return i;
+}
+
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Answer is " + NumDig(num));
+
+*/
+
+
+Console.Write("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a + " ^ " + b + " = " + Math.Pow(a, b));
