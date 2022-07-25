5abe667d72a18fbb0ee6bf77b55a1e4ed9d33ad9
Harmrazer <harmrazer@gmail.com>
2022-07-25 19:56:21 +0300

    L6

---
+++ L6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+            .Replace("(", "")
+            .Replace(")", "")
+            ;
+var data = text.Split(" ")     //var универсальный тип данный; тут вместо var можно было написать string[]
+                .Select(item => item.Split(','))
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                .Where(e => e.x % 2 ==0)
+                .Select(point => (point.x * 10, point.y))
+                .ToArray();
+for (int i = 0; i < data.Length; i++)
+{
+         
+   
+    Console.WriteLine(data[i]);
+}
+
+
