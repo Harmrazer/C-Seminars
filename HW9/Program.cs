4114452fd1cb8ee14a3f2d25efba277e14239979
Harmrazer <harmrazer@gmail.com>
2022-07-28 17:32:35 +0300

    HW9

---
+++ HW9/Program.cs
@@ -0,0 +1,43 @@
+﻿//HomeWork 9
+Console.WriteLine();
+Console.WriteLine("HomeWork 9");
+Console.WriteLine();
+
+// Задача 64: Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+
+Console.WriteLine("Task # 64 ");
+Console.WriteLine();
+
+int NumsOfDigits(int n)
+{
+    if(n > 9)
+    {
+        return 1 + NumsOfDigits(n/10);
+    }
+        else return 1;
+}
+
+Console.WriteLine("Input number N ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Number of digits from {N} is " + NumsOfDigits(N));
+Console.WriteLine();
+
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.WriteLine("Task # 66 ");
+Console.WriteLine();
+
+int SumNumsMN(int m, int n)
+{
+    if(m < n) 
+
+    return m + SumNumsMN(m + 1 , n);
+
+    else return m;
+}
+
+Console.WriteLine("Input number M ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number N ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits from {m} to {n} is " + SumNumsMN(m,n));
---
+++ S9/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+void ShowNums(int n)
+{
+    if(n > 1) ShowNums(n - 1);
+
+    Console.Write(n + " ");
+}
+
+// ShowNums(5);
+
+
+// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int SumOfDigits(int n)
+{
+    if(n >= 10)
+    {
+        return n % 10 + SumOfDigits(n/10);
+    }
+    else return n;
+}
+
+//Console.WriteLine(SumOfDigits(1234));
+
+
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+void ShowNumsMN(int m, int n)
+{
+    if(m <= n) ShowNumsMN(m, n - 1);
+
+    Console.Write(n + " ");
+}
+
+//ShowNumsMN(5, 8);
+// Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+int DegreeOfNums(int a, int b)
+{
+    if (b == 1) return a;
+        else if (b ==0) return 1;
+            else return a * DegreeOfNums(a, b-1);
+} 
+
+// Console.WriteLine(DegreeOfNums(2, 5));
+
+// double stepen(double a, double b)
+// {
+//     if (b > 0)
+//     {
+//         return a * stepen(a, b-1);
+//     }
+//     else if (b < 0)
+//     {
+//         return 1/a * (stepen(a, b + 1));
+//     }
+//     else return 1;
+// }
+// Console.WriteLine(stepen(2, -3));
