bde0a7de6a2e7676afdfa14bca5ee815cb0d24a5
Harmrazer <harmrazer@gmail.com>
2022-08-03 17:50:31 +0300

    HW10

--- HW10/Program.cs
+++ HW10/Program.cs
@@ -1,53 +1,55 @@
-﻿//Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
+﻿//HomeWork 10
+Console.WriteLine();
+Console.WriteLine("HomeWork 10");
+Console.WriteLine();
 
+//Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
+Console.WriteLine("Task # 1 ");
+Console.WriteLine();
 
-// int FindVowel(string[] words, char[] vowel)
-// {       int count = 0;
-//     for (int i = 0; i < words.Length; i++ )
-//         for(int j =0; j < vowel.Length; j++)
-//     {
-//             if (words[i][0] == vowel[j]) count += 1;  
-//     }
+int FindVowel(string[] words, char[] vowel)
+{       int count = 0;
+    for (int i = 0; i < words.Length; i++ )
+        for(int j =0; j < vowel.Length; j++)
+    {
+            if (words[i][0] == vowel[j]) count += 1;  
+    }
 
-//     return count;
-// }
+    return count;
+}
 
-// char[] vowel = {'a', 'e', 'i', 'o', 'u', 'y'};
+char[] vowel = {'a', 'e', 'i', 'o', 'u', 'y'};
 
-// string[] words = {"aqua", "ring", "clock", "use", "lock"};
-// Console.WriteLine("words begoning with vowel letter is " + FindVowel(words, vowel));
+string[] words = {"aqua", "ring", "clock", "use", "lock"};
+Console.WriteLine("words begining with vowel letter is " + FindVowel(words, vowel));
 
 
 //Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
-
-//  string [] names = {"De", "nis", "And", "rew", "rs", "696", "Dmit", "rij", "Iv", "an"};
-
-
-
-// string [] Merge(string [] nam)
-// {
-//     string[] array = new string[nam.Length];
-
-//     for (int i = 0; i < nam.Length-1; i=i+2)
-//     {
-//         array[i] = nam[i] + nam[i+1];
-//     }
-//     return array;
-// }
-
-// void ShowArray( string[] merg)
-// {
-//     for (int i = 0; i < merg.Length; i++)
-//         Console.Write(merg[i] + " ");
-// } 
+Console.WriteLine();
+Console.WriteLine("Task # 2 ");
+Console.WriteLine();
 
 
-// ShowArray(Merge(names));
+ string [] names = {"De", "nis", "And", "rew", "rs", "696", "Dmit", "rij", "Iv", "an"};
 
-// Здравствуйте Павел!
-// Вот ссылка на последнее дз №10. 
-// Спасибо вам большое за интереснейшие и познавательные семинары! Я никогда раньше не кодил, так слегка на втором высшем и то не совсем понимал что делаю (кстати тоже на С#),
-// думал, что программирование это вообще не мое, но Вы изменили мое мнение и у меня стало получатьсяб стал вникать и старался сделать код как можно покороче
-//  и менее ресурсно затратным, а так как смотрел семинары в записи сначала решал задачи сам, а потом смотрел как решили другие.
-// Сейчас появилось желание писать, так как стал понимать что вообще делаю и откуда что берется, ошибки научился определять быстро и исправлять. 
-// Вообщем привили интерес и дали понимание в программировании, спасибо Вам еще раз за это большое! 
+string [] Merge(string [] nam)
+{
+    string[] array = new string[nam.Length];
+
+    for (int i = 0; i < nam.Length-1; i=i+2)
+    {
+        array[i] = nam[i] + nam[i+1];
+    }
+    return array;
+}
+
+void ShowArray( string[] merg)
+{
+    for (int i = 0; i < merg.Length; i++)
+        Console.Write(merg[i] + " ");
+} 
+
+
+ShowArray(Merge(names));
+Console.WriteLine();
+
